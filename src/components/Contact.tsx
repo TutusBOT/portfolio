cce801b5d9846975a8022c2730c2ff8839aecf61
TutusBOT <barte77pl@gmail.com>
2023-02-02 23:28:01 +0100

    contact component styling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,35 +4,28 @@ import Input from "./Input";
 const Contact = () => {
 	return (
 		<section
-			className="bg-purple relative pb-8 flex flex-col items-center gap-8"
+			className="bg-[#f5f5f5] py-8 px-2 flex flex-col items-center gap-8"
 			id="contact"
 		>
-			<div className="absolute w-0 h-0 border-t-black border-x-transparent border-b-transparent border-t-[100px] border-r-[100vw] border-l-0 border-b-0"></div>
-			<h2 className="text-white text-4xl text-center mt-[120px] w-max relative after:absolute after:top-full after:left-0 after:w-full after:h-1 after:bg-black after:content-['']">
+			<h2 className="text-black text-4xl text-center w-max relative after:absolute after:top-full after:left-0 after:w-full after:h-1 after:bg-purple after:content-['']">
 				Let&#39;s get in touch
 			</h2>
-			<div className="grid grid-cols-2">
-				<div>bartlomiej.tutak0@gmail.com</div>
-				<form action="return" className="flex flex-col px-8 items-center">
-					<div className="flex">
-						<div className="flex flex-col">
-							<Input label="Full Name" id="name" />
-						</div>
-						<div className="flex flex-col">
-							<Input type="email" label="Email" id="email"></Input>
-						</div>
-					</div>
-					<label htmlFor="message" className="text-xl">
-						Message
-					</label>
+			<form
+				action="return"
+				className="flex flex-col px-4 sm:px-8 py-4 sm:py-8 items-center w-full max-w-3xl gap-4 bg-black-light shadow-2xl"
+			>
+				<Input label="Full Name" id="name" />
+				<Input type="email" label="Email" id="email"></Input>
+				<div className="flex flex-col max-w-3xl w-full">
+					<label htmlFor="message">Message</label>
 					<textarea
 						id="message"
-						rows={3}
-						className="outline-none resize-y bg-transparent border-black border-2 text-xl p-2 w-[720px]"
+						rows={5}
+						className="outline-none resize-y bg-transparent border-white border-2 text-lg p-2 w-full"
 					></textarea>
-					<Button>SEND</Button>
-				</form>
-			</div>
+				</div>
+				<Button className="self-end">SEND</Button>
+			</form>
 		</section>
 	);
 };
