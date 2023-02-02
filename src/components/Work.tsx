3b88f10e6cfa91a09304751d9365f1173d3963a5
TutusBOT <barte77pl@gmail.com>
2023-02-02 23:28:22 +0100

    added entry animation

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,26 +1,40 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useInView, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 import Project, { ProjectI } from "./Project";
 
 const Work = () => {
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true });
+	const [entryAnimation, setEntryAnimation] = useState({});
+
+	useEffect(() => {
+		if (isInView) {
+			setEntryAnimation({ opacity: 1, translateY: 0 });
+		}
+	}, [isInView]);
+
 	const projects = useQuery(["projects"], () =>
 		axios.get("http://192.168.1.10:1338/projects")
 	);
 
 	return (
-		<section
-			className="w-full max-w-full flex flex-col gap-16 px-8 py-24"
+		<motion.section
+			className="w-full max-w-full flex flex-col gap-16 px-8 py-24 duration-500 translate-y-1/3"
 			id="work"
+			animate={entryAnimation}
 		>
-			<h2 className="text-4xl text-center flex justify-center">
+			<h2 className="text-4xl text-center flex justify-center" ref={ref}>
 				<span className="w-max relative after:absolute after:top-[110%] after:left-0 after:w-full after:h-1 after:bg-purple after:content-['']">
 					My projects
 				</span>
 			</h2>
-			<p className="text-center text-xl">
-				Here you will find some of my personal projects that I created. Each one
-				of them is self-hosted on Linux using <strong>Docker</strong>.
+			<p className="text-center text-xl whitespace-pre-line 2xl:whitespace-normal">
+				Here you will find some of my personal projects that I created. {"\n"}{" "}
+				Each one of them is hosted using <strong>Docker</strong> and{" "}
+				<strong>Docker Compose</strong>.
 			</p>
 			<div className="grid items-center justify-center md:px-16">
 				{projects.data
@@ -40,7 +54,7 @@ const Work = () => {
 					  })
 					: null}
 			</div>
-		</section>
+		</motion.section>
 	);
 };
 export default Work;
