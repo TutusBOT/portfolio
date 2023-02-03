ee8c05ac44450298aabd3ad521a529e55b00dd51
TutusBOT <barte77pl@gmail.com>
2023-02-03 15:12:54 +0100

    added attributes necessary for form handling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,14 @@
 interface Input {
 	label: string;
 	id: string;
+	name: string;
 	type?: string;
+	required?: boolean;
+	value: string | number | readonly string[] | undefined;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ label, id, type }: Input) => {
+const Input = ({ label, id, type, name, required, value, onChange }: Input) => {
 	return (
 		<div className="flex flex-col max-w-3xl w-full">
 			<label htmlFor={id}>{label}</label>
@@ -12,6 +16,10 @@ const Input = ({ label, id, type }: Input) => {
 				id={id}
 				type={type || "text"}
 				className="bg-transparent border-white border-2 outline-none text-lg p-2 w-full"
+				name={name}
+				required={required}
+				value={value}
+				onChange={(e) => onChange(e)}
 			></input>
 		</div>
 	);
