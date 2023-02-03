1b58dba344d1fe5e95ea51e298d9f016757b16dd
TutusBOT <barte77pl@gmail.com>
2023-02-03 15:12:12 +0100

    added button types

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,13 @@
 interface Button {
 	children?: React.ReactNode;
 	className?: string;
+	type?: "button" | "submit" | "reset";
 }
 
-const Button = ({ className, children }: Button) => {
+const Button = ({ className, children, type }: Button) => {
 	return (
 		<button
+			type={type || "button"}
 			className={`bg-purple-light text-lg text-white uppercase min-w-[120px] w-min py-2 ${className}`}
 		>
 			{children}
