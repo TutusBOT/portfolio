e3232bc5106f27edd18bbc2fbfff4173c86f13ec
TutusBOT <barte77pl@gmail.com>
2023-05-21 23:56:56 +0200

    fix: scrolling feat: add padding

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -22,76 +22,80 @@ const About = () => {
 	);
 
 	return (
-		<motion.section
-			id="about"
-			className="flex flex-col items-center gap-16 py-24 px-8 opacity-0 duration-500 translate-y-1/3"
-			animate={entryAnimation}
-		>
-			<h2
-				className="w-max text-4xl text-center relative after:absolute after:top-full after:left-0 after:w-full after:h-1 after:bg-purple after:content-['']"
-				ref={ref}
+		<>
+			<div id="about"></div>
+			<motion.section
+				className="flex flex-col items-center gap-16 py-32 px-8 opacity-0 duration-500 translate-y-1/3"
+				animate={entryAnimation}
 			>
-				About me
-			</h2>
-			<p className="text-xl -mt-8 text-center">
-				{about.data ? (
-					about.data.data.short_description
-				) : (
-					<Skeleton className="w-[50ch] h-5" />
-				)}
-			</p>
-			<div className="grid md:grid-cols-2 gap-32">
-				<div className="flex flex-col gap-6 md:max-w-lg">
-					<h3 className="text-2xl">Get to know me</h3>
-					<p>
-						I&#39;m a <strong>Web Developer</strong> that can bring life to your
-						project with <span className="text-purple-light">&lt;code&gt;</span>{" "}
-						and <i>passion</i>.
-					</p>
-					<p>
-						I&#39;m open to <strong>job</strong> opportunities where I can
-						constantly learn and grow. If my skills match your expectations
-						don&#39;t hesitate to <strong>contact</strong> me.
-					</p>
-					<Link
-						text="CONTACT"
-						url="#contact"
-						className="max-w-[200px]"
-						target="_self"
-					/>
+				<h2
+					className="w-max text-4xl text-center relative after:absolute after:top-full after:left-0 after:w-full after:h-1 after:bg-purple after:content-['']"
+					ref={ref}
+				>
+					About me
+				</h2>
+				<p className="text-xl -mt-8 text-center">
+					{about.data ? (
+						about.data.data.short_description
+					) : (
+						<Skeleton className="w-[50ch] h-5" />
+					)}
+				</p>
+				<div className="grid md:grid-cols-2 gap-32">
+					<div className="flex flex-col gap-6 md:max-w-lg">
+						<h3 className="text-2xl">Get to know me</h3>
+						<p>
+							I&#39;m a <strong>Web Developer</strong> that can bring life to
+							your project with{" "}
+							<span className="text-purple-light">&lt;code&gt;</span> and{" "}
+							<i>passion</i>.
+						</p>
+						<p>
+							I&#39;m open to <strong>job</strong> opportunities where I can
+							constantly learn and grow. If my skills match your expectations
+							don&#39;t hesitate to <strong>contact</strong> me.
+						</p>
+						<p>In my spare time, I enjoy playing computer games and chess.</p>
+						<Link
+							text="CONTACT"
+							url="#contact"
+							className="max-w-[200px]"
+							target="_self"
+						/>
+					</div>
+					<div className="md:max-w-lg flex flex-col gap-6">
+						<h3 className="text-2xl">My skills</h3>
+						<ul className="max-w-full flex flex-wrap gap-3">
+							{about.data ? (
+								about.data.data.skills.map((skill: string) => {
+									return (
+										<li
+											key={skill}
+											className="bg-black-light px-4 py-2 text-sm lg:text-lg w-max"
+										>
+											{skill}
+										</li>
+									);
+								})
+							) : (
+								<>
+									<Skeleton className="h-8 w-20" />
+									<Skeleton className="h-8 w-20" />
+									<Skeleton className="h-8 w-20" />
+									<Skeleton className="h-8 w-20" />
+									<Skeleton className="h-8 w-20" />
+									<Skeleton className="h-8 w-20" />
+									<Skeleton className="h-8 w-20" />
+									<Skeleton className="h-8 w-20" />
+									<Skeleton className="h-8 w-20" />
+									<Skeleton className="h-8 w-20" />
+								</>
+							)}
+						</ul>
+					</div>
 				</div>
-				<div className="md:max-w-lg flex flex-col gap-6">
-					<h3 className="text-2xl">My skills</h3>
-					<ul className="max-w-full flex flex-wrap gap-3">
-						{about.data ? (
-							about.data.data.skills.map((skill: string) => {
-								return (
-									<li
-										key={skill}
-										className="bg-black-light px-4 py-2 text-sm lg:text-lg w-max"
-									>
-										{skill}
-									</li>
-								);
-							})
-						) : (
-							<>
-								<Skeleton className="h-8 w-20" />
-								<Skeleton className="h-8 w-20" />
-								<Skeleton className="h-8 w-20" />
-								<Skeleton className="h-8 w-20" />
-								<Skeleton className="h-8 w-20" />
-								<Skeleton className="h-8 w-20" />
-								<Skeleton className="h-8 w-20" />
-								<Skeleton className="h-8 w-20" />
-								<Skeleton className="h-8 w-20" />
-								<Skeleton className="h-8 w-20" />
-							</>
-						)}
-					</ul>
-				</div>
-			</div>
-		</motion.section>
+			</motion.section>
+		</>
 	);
 };
 export default About;
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -22,45 +22,45 @@ const Work = () => {
 	);
 
 	return (
-		<motion.section
-			className="w-full max-w-full flex flex-col gap-16 px-8 py-24 duration-500 translate-y-1/3"
-			id="work"
-			animate={entryAnimation}
-		>
-			<h2 className="text-4xl text-center flex justify-center" ref={ref}>
-				<span className="w-max relative after:absolute after:top-[110%] after:left-0 after:w-full after:h-1 after:bg-purple after:content-['']">
-					My projects
-				</span>
-			</h2>
-			<p className="text-center text-xl whitespace-pre-line 2xl:whitespace-normal">
-				Here you will find some of my personal projects that I created. {"\n"}{" "}
-				Each one of them is hosted using <strong>Docker</strong> and{" "}
-				<strong>Docker Compose</strong>.
-			</p>
-			<div className="grid grid-cols-1 sm:grid-cols-2 items-center justify-items-center gap-8 md:px-16">
-				{projects.data ? (
-					projects.data.data
-						.sort((a: ProjectI, b: ProjectI) => a.importance - b.importance)
-						.map((project: ProjectI) => {
-							return (
-								<Project
-									key={project.id}
-									description={project.description}
-									image={project.image}
-									title={project.title}
-									technologies={project.technologies}
-									github={project.github}
-									live={project.live}
-									id={project.id}
-									importance={project.importance}
-								/>
-							);
-						})
-				) : (
-					<Skeleton className="w-[300px] md:w-[450px] max-w-md h-[450px]" />
-				)}
-			</div>
-		</motion.section>
+		<>
+			<div id="work"></div>
+			<motion.section
+				className="w-full max-w-full flex flex-col gap-16 px-8 py-32 duration-500 translate-y-1/3"
+				animate={entryAnimation}
+			>
+				<h2 className="text-4xl text-center flex justify-center" ref={ref}>
+					<span className="w-max relative after:absolute after:top-[110%] after:left-0 after:w-full after:h-1 after:bg-purple after:content-['']">
+						My projects
+					</span>
+				</h2>
+				<p className="text-center text-xl whitespace-pre-line 2xl:whitespace-normal">
+					Here you will find some of my personal projects that I created. {"\n"}{" "}
+				</p>
+				<div className="grid grid-cols-1 sm:grid-cols-2 items-center justify-items-center gap-8 md:px-16">
+					{projects.data ? (
+						projects.data.data
+							.sort((a: ProjectI, b: ProjectI) => a.importance - b.importance)
+							.map((project: ProjectI) => {
+								return (
+									<Project
+										key={project.id}
+										description={project.description}
+										image={project.image}
+										title={project.title}
+										technologies={project.technologies}
+										github={project.github}
+										live={project.live}
+										id={project.id}
+										importance={project.importance}
+									/>
+								);
+							})
+					) : (
+						<Skeleton className="w-[300px] md:w-[450px] max-w-md h-[450px]" />
+					)}
+				</div>
+			</motion.section>
+		</>
 	);
 };
 export default Work;
